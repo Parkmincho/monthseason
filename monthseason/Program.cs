2f8825ce5c6cfaf2f62ea5b7cbccd598d3b2279d
Mincho Park <s2018s28@e-mirim.hs.kr>
2020-05-06 21:51:30 +0900

    switch case문

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,7 +10,37 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
-            
+            Console.ReadLine();
+            Console.Write("이번 달은 몇 월인가요 : ");
+            var input = Console.ReadLine();
+
+
+            switch (input)
+            {
+                case "12월": 
+                case "1월":
+                case "2월": 
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case "3월":
+                case "4월":
+                case "5월":
+                    Console.WriteLine("봄입니다");
+                    break;
+                case "6월":
+                case "7월":
+                case "8월":
+                    Console.WriteLine("여름입니다");
+                    break;
+                case "9월":
+                case "10월":
+                case "11월":
+                    Console.WriteLine("가을입니다");
+                    break;
+                default:
+                    Console.WriteLine("잘못입력하였습니다");
+                    break;
+            }
         }
     }
 }
